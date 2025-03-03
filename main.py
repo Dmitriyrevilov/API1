0f9a9fcb87fda8fb39013df19a10ad25b4d87757
Dmitriyrevilov <dimrevilov@gmail.com>
2025-03-03 11:37:08 +0300

    new commit

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import requests
+import urllib.parse
+from urllib.parse import urljoin
 
 
 def get_weather_of_places(place):
-    url = "http://wttr.in/{}?M?n/Tqu&lang=ru"
-    url = url.format([place])
+    base_url = "http://wttr.in/"
+    params = {
+        "M": "",
+        "n": "",
+        "Tqu": "",
+        "lang": "ru",
+    }
+
+    url = urljoin(base_url, place) + "?" + urllib.parse.urlencode(params)
     response = requests.get(url)
     response.raise_for_status()
     return response.text
 
 
 def main():
-    wheather_places = ["~Череповец", "~paris", "~svo"]
-    for place in wheather_places:
+    places = [
+        "~Череповец",
+        "~paris",
+        "~svo",
+    ]
+    for place in places:
         print(get_weather_of_places(place))
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
